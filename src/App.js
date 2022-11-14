d02ea40d2764a3bd57c8532c92a4fa7546c488e9
Pavel <paha_gagarin@mail.ru>
2022-11-14 10:16:35 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React from 'react';
+import { Routes, Route} from 'react-router-dom';
+import HomePage from './pages/homePage/HomePage';
+import GamePage from './pages/gamePage/GamePage';
+import Header from './components/header/Header';
+import OrderPage from './pages/orderPage/OrderPage';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<HomePage></HomePage>}></Route>
+        <Route path='/app/:title' element={<GamePage></GamePage>}></Route>
+        <Route path='/order' element={<OrderPage></OrderPage>}></Route>
+      </Routes>
     </div>
   );
 }
